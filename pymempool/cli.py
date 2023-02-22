b4b4d9d6f3fc6200581fb781684b4ec4363560db
holgern <holgern@users.noreply.github.com>
2023-02-22 11:23:12 +0100

    Use toml and add circleci

---
+++ pymempool/cli.py
@@ -0,0 +1,34 @@
+import logging
+
+import click
+import typer
+from rich.console import Console
+from rich.table import Table
+
+
+log = logging.getLogger(__name__)
+app = typer.Typer()
+console = Console()
+
+state = {"verbose": 3}
+
+
+@app.callback()
+def main(verbose: int = 3):
+    """Python CLI for mempool.space, enjoy."""
+    # Logging
+    state["verbose"] = verbose
+    log = logging.getLogger(__name__)
+    verbosity = ["critical", "error", "warn", "info", "debug"][int(min(verbose, 4))]
+    log.setLevel(getattr(logging, verbosity.upper()))
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    ch = logging.StreamHandler()
+    ch.setLevel(getattr(logging, verbosity.upper()))
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
+
+if __name__ == "__main__":
+    app()
--- setup.py
+++ setup.py
@@ -1,34 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.2'
-
-tests_require = ['pytest', 'responses']
-
-requires = [
-    "urllib3",
-    "requests",
-]
-
-if __name__ == '__main__':
-    setup(
-        name='pymempool',
-        version=VERSION,
-        description='Python Api for mempool.space',
-        long_description=open('README.md').read(),
-        long_description_content_type="text/markdown",
-        author='Holger Nahrstaedt',
-        author_email='nahrstaedt@gmail.com',
-        url='http://www.github.com/holgern/pymempool',
-        keywords=['btc', 'mempool'],
-        packages=[
-            "pymempool",
-        ],
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-        ],
-        install_requires=requires,
-        tests_require=tests_require,
-    )
+setup(
+    use_scm_version={'write_to': 'pymempool/_version.py'},
+    setup_requires=['setuptools_scm'],
+)
